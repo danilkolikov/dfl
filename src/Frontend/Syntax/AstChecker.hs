e4382d06c8cba8b6430689b7211e3593fdf5c1d1
danilkolikov <danilkolikov@gmail.com>
2019-05-26 19:37:41 +0100

    Check that last statement in `do` is an expression
    - A missing check that the last statement in `do` is an expression and 
    not binding or `let` was added

--- src/Frontend/Syntax/AstChecker.hs
+++ src/Frontend/Syntax/AstChecker.hs
@@ -21,9 +21,10 @@ import Frontend.Syntax.Ast
 import Frontend.Syntax.Position (WithLocation(..))
 
 -- | Errors which can found by checking AST
-data AstCheckerError =
-    AstCheckerErrorSameTyVar (WithLocation TyVar)
-                             (WithLocation TyVar) -- ^ Some type variables in instance declaration are same
+data AstCheckerError
+    = AstCheckerErrorSameTyVar (WithLocation TyVar)
+                               (WithLocation TyVar) -- ^ Some type variables in instance declaration are same
+    | AstCheckerErrorLastStatement (WithLocation Stmt) -- ^ Last statement in Do is let or assignment
     deriving (Show, Eq)
 
 -- | Type for checker of AST
@@ -56,7 +57,9 @@ instance AstCheckable Body where
     checkAst (Body _ topDecl) = checkAst topDecl
 
 instance AstCheckable TopDecl where
-    checkAst (TopDeclInstance _ _ inst _) = checkAst inst
+    checkAst (TopDeclInstance _ _ inst decls) = checkAst inst >> checkAst decls
+    checkAst (TopDeclClass _ _ _ decls) = checkAst decls
+    checkAst (TopDeclDecl decl) = checkAst decl
     checkAst _ = correct
 
 instance AstCheckable Inst where
@@ -64,6 +67,85 @@ instance AstCheckable Inst where
     checkAst (InstTuple f s rest) = checkDifferentTyVars (f : s : rest)
     checkAst (InstFunction from to) = checkDifferentTyVars [from, to]
     checkAst _ = correct
+
+instance AstCheckable Decl where
+    checkAst DeclGenDecl {} = correct
+    checkAst (DeclFunction _ rhs) = checkAst rhs
+
+instance AstCheckable CDecl where
+    checkAst CDeclGenDecl {} = correct
+    checkAst (CDeclFunction _ rhs) = checkAst rhs
+
+instance AstCheckable IDecl where
+    checkAst (IDeclFunction _ rhs) = checkAst rhs
+
+instance AstCheckable RHS where
+    checkAst (RHSSimple exp' decls) = checkAst exp' >> checkAst decls
+    checkAst (RHSGuarded gdrhs decls) = checkAst gdrhs >> checkAst decls
+
+instance AstCheckable GdRHS where
+    checkAst (GdRHS guards exp') = checkAst guards >> checkAst exp'
+
+instance AstCheckable Guard where
+    checkAst (GuardPattern _ exp') = checkAst exp'
+    checkAst (GuardLet decls) = checkAst decls
+    checkAst (GuardExpr e) = checkAst e
+
+instance AstCheckable Exp where
+    checkAst (ExpSimple e) = checkAst e
+    checkAst (ExpTyped e _ _) = checkAst e
+
+instance AstCheckable InfixExp where
+    checkAst (InfixExpApplication l _ r) = checkAst l >> checkAst r
+    checkAst (InfixExpNegated _ exp') = checkAst exp'
+    checkAst (InfixExpLExp exp') = checkAst exp'
+
+instance AstCheckable LExp where
+    checkAst (LExpAbstraction _ exp') = checkAst exp'
+    checkAst (LExpLet decls exp') = checkAst decls >> checkAst exp'
+    checkAst (LExpIf cond true false) = mapM_ checkAst [cond, true, false]
+    checkAst (LExpCase exp' alts) = checkAst exp' >> checkAst alts
+    checkAst (LExpDo stmts) = checkLastStatement >> checkAst stmts
+      where
+        checkLastStatement :: AstChecker
+        checkLastStatement =
+            let lastStatement = NE.last stmts
+             in case getValue lastStatement of
+                    StmtExp _ -> correct
+                    _ -> Left $ AstCheckerErrorLastStatement lastStatement
+    checkAst (LExpApplication exps) = checkAst exps
+
+instance AstCheckable AExp where
+    checkAst (AExpParens exp') = checkAst exp'
+    checkAst (AExpTuple f s rest) = mapM_ checkAst (f : s : rest)
+    checkAst (AExpList exps) = checkAst exps
+    checkAst (AExpSequence s f e) = checkAst s >> checkAst f >> checkAst e
+    checkAst (AExpListCompr exp' quals) = checkAst exp' >> checkAst quals
+    checkAst (AExpLeftSection exp' _) = checkAst exp'
+    checkAst (AExpRightSection _ exp') = checkAst exp'
+    checkAst (AExpRecordUpdate exp' binds) = checkAst exp' >> checkAst binds
+    checkAst (AExpRecordConstr _ binds) = checkAst binds
+    checkAst _ = correct
+
+instance AstCheckable Alt where
+    checkAst (AltSimple _ exp' decls) = checkAst exp' >> checkAst decls
+    checkAst (AltGuarded _ gdpat decls) = checkAst gdpat >> checkAst decls
+
+instance AstCheckable Qual where
+    checkAst (QualGenerator _ exp') = checkAst exp'
+    checkAst (QualLet decls) = checkAst decls
+    checkAst (QualGuard exp') = checkAst exp'
+
+instance AstCheckable Stmt where
+    checkAst (StmtPat _ exp') = checkAst exp'
+    checkAst (StmtLet decls) = checkAst decls
+    checkAst (StmtExp exp') = checkAst exp'
+
+instance AstCheckable GdPat where
+    checkAst (GdPat guards exp') = checkAst guards >> checkAst exp'
+
+instance AstCheckable FBind where
+    checkAst (FBind _ exp') = checkAst exp'
 
 -- Helper functions
 correct :: AstChecker
--- test/Frontend/Syntax/AstCheckerTest.hs
+++ test/Frontend/Syntax/AstCheckerTest.hs
@@ -10,10 +10,16 @@ module Frontend.Syntax.AstCheckerTest where
 
 import Test.Hspec
 
+import qualified Data.List.NonEmpty as NE (NonEmpty(..))
+
 import Frontend.Syntax.Ast
 import Frontend.Syntax.AstChecker
-import Frontend.Syntax.Position (WithLocation(..), sourceLocation)
-import Frontend.Syntax.Token (VarId(..))
+import Frontend.Syntax.Position
+    ( WithLocation(..)
+    , sourceLocation
+    , withDummyLocation
+    )
+import Frontend.Syntax.Token (IntT(..), VarId(..))
 
 testSuite :: IO ()
 testSuite =
@@ -31,7 +37,7 @@ testSuite =
                 Left (AstCheckerErrorSameTyVar (tyVar "x" 4) (tyVar "x" 6))
             checkAst (InstFunction (tyVar "x" 4) (tyVar "x" 6)) `shouldBe`
                 Left (AstCheckerErrorSameTyVar (tyVar "x" 4) (tyVar "x" 6))
-        it "doesn't rise error when all type variables are different" $ do
+        it "doesn't raise error when all type variables are different" $ do
             checkAst
                 (InstNamed
                      (WithLocation GTyConUnit (sourceLocation 1 1 1 3))
@@ -42,3 +48,28 @@ testSuite =
             checkAst (InstFunction (tyVar "x" 4) (tyVar "y" 6)) `shouldBe`
                 Right ()
             checkAst (InstList (tyVar "x" 4)) `shouldBe` Right ()
+        let aExp =
+                withDummyLocation .
+                AExpLiteral .
+                withDummyLocation . LiteralInteger . withDummyLocation . IntT $
+                42
+            exp' =
+                withDummyLocation .
+                ExpSimple . withDummyLocation . InfixExpLExp . withDummyLocation $
+                LExpApplication (aExp NE.:| [])
+        it "raises error when the last statement in do is not an expression" $ do
+            let pat =
+                    withDummyLocation .
+                    PatSimple .
+                    withDummyLocation .
+                    LPatNegated . withDummyLocation . Left . IntT $
+                    42
+                stmt1 = withDummyLocation $ StmtPat pat exp'
+            checkAst (LExpDo (stmt1 NE.:| [])) `shouldBe`
+                Left (AstCheckerErrorLastStatement stmt1)
+            let stmt2 = withDummyLocation $ StmtLet []
+            checkAst (LExpDo (stmt2 NE.:| [])) `shouldBe`
+                Left (AstCheckerErrorLastStatement stmt2)
+        it "doesn't raise error when the last statement in do is an expression" $ do
+            let stmt = withDummyLocation $ StmtExp exp'
+            checkAst (LExpDo (stmt NE.:| [])) `shouldBe` Right ()
