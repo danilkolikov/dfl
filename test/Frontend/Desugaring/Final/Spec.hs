40a379cf8581abf18ce1a04404e424dcf89f82f9
danilkolikov <danilkolikov@gmail.com>
2019-06-01 23:22:33 +0100

    Final desugaring of type synonyms
    - Final desugaring of type synonyms was added
    - A test for it was added as well

--- test/Frontend/Desugaring/Final/Spec.hs
+++ test/Frontend/Desugaring/Final/Spec.hs
@@ -11,7 +11,9 @@ module Frontend.Desugaring.Final.Spec
     ) where
 
 import qualified Frontend.Desugaring.Final.ProcessorTest as Processor
+import qualified Frontend.Desugaring.Final.TypeSynonymDesugaringTest as TypeSynonym
 
 testSuite :: IO ()
 testSuite = do
     Processor.testSuite
+    TypeSynonym.testSuite
